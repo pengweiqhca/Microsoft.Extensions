1c5b6426ec2924812cfbe741d9686922252ce086
Hao Kung <HaoK@users.noreply.github.com>
2018-11-27 00:35:50 -0800

    Working tests

--- test/vstest/runtests.cmd
+++ test/vstest/runtests.cmd
@@ -1,9 +1,7 @@
 set target=%1
-set cliVersion=%2
-powershell.exe -NoProfile -NoLogo -ExecutionPolicy unrestricted -Command "./dotnet-install.ps1 -Version %version% -Channel 3.0 -Runtime dotnet -NoCdn -InstallDir %HELIX_CORRELATION_PAYLOAD%"
+set version=%2
+powershell.exe -NoProfile -NoLogo -ExecutionPolicy unrestricted -Command "./dotnet-install.ps1 -Version %version% -Channel 2.0 -NoCdn -InstallDir %HELIX_CORRELATION_PAYLOAD%"
 set PATH=%HELIX_CORRELATION_PAYLOAD%;%PATH%
 dotnet vstest %target% --logger:trx
-dir
-dir %HELIX_CORRELATION_PAYLOAD%\shared\Microsoft.NETCore.App
 
 
