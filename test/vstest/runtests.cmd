f485c60aae4a7a09392408851a92173fc002ec11
Nate McMaster <natemcmaster@users.noreply.github.com>
2018-11-28 23:19:13 -0800

    Update test/vstest/runtests.cmd
    Co-Authored-By: HaoK <HaoK@users.noreply.github.com>

--- test/vstest/runtests.cmd
+++ test/vstest/runtests.cmd
@@ -1,5 +1,6 @@
 set target=%1
 set version=%2
+powershell.exe -NoProfile -NoLogo -ExecutionPolicy unrestricted -Command "Invoke-WebRequest https://get.dot.net/v1/dotnet-install.ps1 -o ./dotnet-install.ps1"
 powershell.exe -NoProfile -NoLogo -ExecutionPolicy unrestricted -Command "./dotnet-install.ps1 -Version %version% -Channel 2.0 -NoCdn -InstallDir %HELIX_CORRELATION_PAYLOAD%"
 set PATH=%HELIX_CORRELATION_PAYLOAD%;%PATH%
 set DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
