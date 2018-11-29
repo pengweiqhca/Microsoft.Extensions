1f37d9c0d48c4f53914596a0f239983091debd35
Nate McMaster <natemcmaster@users.noreply.github.com>
2018-11-28 23:17:35 -0800

    Update test/vstest/runtests.cmd
    Co-Authored-By: HaoK <HaoK@users.noreply.github.com>

--- test/vstest/runtests.cmd
+++ test/vstest/runtests.cmd
@@ -2,6 +2,7 @@ set target=%1
 set version=%2
 powershell.exe -NoProfile -NoLogo -ExecutionPolicy unrestricted -Command "./dotnet-install.ps1 -Version %version% -Channel 2.0 -NoCdn -InstallDir %HELIX_CORRELATION_PAYLOAD%"
 set PATH=%HELIX_CORRELATION_PAYLOAD%;%PATH%
+set DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
 dotnet vstest %target% --logger:trx
 
 
